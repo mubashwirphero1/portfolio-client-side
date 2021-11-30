9295afb5a2d04b4a3a53c04aa48984e150c85c7a
mubashwirphero1 <86701680+mubashwirphero1@users.noreply.github.com>
2021-11-30 22:01:25 +0600

    Navigation and Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Components/Static/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Shared/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-900 border-t p-4 text-center">
+            <h3 className="text-gray-400">
+                Developed by Hasan Al Mubashwir &nbsp;&nbsp;&nbsp;&nbsp;
+                &copy; Copyright 2021-2022
+            </h3>
+        </div>
+    );
+};
+
+export default Footer;
